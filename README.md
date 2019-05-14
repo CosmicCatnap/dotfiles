518b385b4eeae526fe0d3abea0eac5f3e6c38ee3
Cosmic Catnap <cosmiccatnap@gmail.com>
2019-05-14 19:36:19 -0400

    revised for 2019

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # dotfiles
-My dotfiles for anybody for any reason, go nuts. 
+##These are my dot files
 
-NOTE: I use oh-my-zsh (https://github.com/robbyrussell/oh-my-zsh) and i3pystatus(https://github.com/enkore/i3pystatus) which are reqiured for these configurations to work correctly. All other packages and files can be installed with `bootstrap.sh` 
-Keep in mind `bootstrap.sh` only works for `Arch Linux` and I have no plans to extend that support to other distributions but a quick look at the script should tell you what packages are used.  
+##For anybody to use 
+
+##For any reason
+
+##Go nuts 
+
+go ahead
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# PLEASE RUN THIS SCRIPT AS THE USER YOU ARE INSTALLING FOR ONLY (NO SUDO PLZ)
+packages='vim rxvt-unicode tmux ttf-dejavu curl wget'
+      printf  "===================================\n"
+      echo -n "Welcome the dotfile install script"
+      printf  "\n==================================\n\n"
+      echo -e "The following packages are required: \033[31m$packages\033[0m"
+      read -p "\nWould you like to install these packages now? " -n 1 -r
+      if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+        printf "\n"
+        sudo pacman -S $packages
+        printf "\n"
+      fi
+      printf "\nWould you like to move dotfiles from \033[31m${PWD}\033[0m to \033[31m$HOME\033[0m?\n"
+      read -p  "(y/n) " -n 1 -r
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+	printf "\nMoving dotfiles from \033[31m${PWD}\033[0m to \033[31m$HOME\033[0m\n"
+	cp -r dots/*  ~/
+	fi
+        echo "Dotfiles install complete!"
