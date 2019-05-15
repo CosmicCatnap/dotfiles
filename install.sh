a411d5f757f1103db6d1fa0bae065e43fd749370
Cosmic Catnap <cosmiccatnap@gmail.com>
2019-05-14 20:18:15 -0400

    refactor

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 # PLEASE RUN THIS SCRIPT AS THE USER YOU ARE INSTALLING FOR ONLY (NO SUDO PLZ)
 packages='vim rxvt-unicode tmux ttf-dejavu curl wget'
       printf  "===================================\n"
       echo -n "Welcome the dotfile install script"
       printf  "\n==================================\n\n"
       echo -e "The following packages are required: \033[31m$packages\033[0m"
-      read -p "\nWould you like to install these packages now? " -n 1 -r
+      read -p "Would you like to install these packages now? " -n 1 -r
       if [[ $REPLY =~ ^[Yy]$ ]]
         then
         printf "\n"
@@ -18,6 +17,7 @@ packages='vim rxvt-unicode tmux ttf-dejavu curl wget'
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 	printf "\nMoving dotfiles from \033[31m${PWD}\033[0m to \033[31m$HOME\033[0m\n"
-	cp -r dots/*  ~/
+        shopt -s dotglob nullglob
+        cp -r dots/* ~/
 	fi
         echo "Dotfiles install complete!"
